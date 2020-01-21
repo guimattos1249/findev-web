c540f312060e7c282b206f51a8fbfd1e63edd78d
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-01-21 19:40:35 -0300

    Finished sytle of sidebar and started styling dev-item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
 
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+
+
 function App() {
   return (
     <div id="app">
       <aside>
-        
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+
       </aside>
       <main>
-
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/50272051?s=460&v=4" alt="Guilherme Mattos"/>
+              <div className="user-info">
+                <strong>Guilherme MAttos</strong>
+                <span>React, Vue, Node</span>
+              </div>
+            </header>
+          </li>
+        </ul>
       </main>
     </div>
   );
