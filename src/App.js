48cc93503f3b6e77d481681dd430d34847834020
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-01-21 22:22:22 -0300

    almost kk, error in show recently added dev :/

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   async function handleAddDev(data) {
     const response = await api.post('/devs', data);
 
-    // setDevs([...devs, response.data]);
+    setDevs([...devs, response.data]);
   }
 
   return (
